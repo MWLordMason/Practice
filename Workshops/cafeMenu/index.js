e6ab45d519edccd09e40cd86f40c3fd344db870d
mwlordmason <mason.walker1709@gmail.com>
2023-05-19 13:07:37 -0600

    commit

---
+++ Workshops/cafeMenu/index.js
@@ -0,0 +1,21 @@
+const coffeeMenu = require("./coffee_data.js")
+// console.log(coffeeMenu)
+
+
+/*Step 2*/
+// coffeeMenu.forEach(element => console.log(element))
+
+/*Step 3*/
+// function fiveOrLess () {
+//     let cheapDrinks = []
+//     for (let i = 0; i < coffeeMenu.length; i++) {
+//         if (coffeeMenu[i].price <= 5) {
+//             cheapDrinks.push(coffeeMenu[i])
+//         }
+//     }
+//     return cheapDrinks
+// }
+
+// console.log(fiveOrLess())
+
+/*Step 4*/
